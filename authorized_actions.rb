510b4466353fd6053778651e9123c819976c0461
Theodore Kimble <mail@theodorekimble.com>
2015-10-23 10:57:34 -0700

    Limit access to authorized git actions and users

--- authorized_actions.rb
+++ authorized_actions.rb
@@ -1,35 +1,46 @@
 #!/usr/bin/ruby
 
-unless ENV['SSH_CONNECTION']
-  puts 'Only ssh allowed'
-  exit
-end
-
 require 'sequel'
 require 'shellwords'
 
+if ENV['SSH_CONNECTION'].to_s.empty?
+  abort 'Only SSH Connections Allowed'
+end
+
+if !ARGV.is_a?(Array) || ARGV.length != 1
+  abort 'Expected Single Argument Array'
+end
+
+user_id = ARGV[0].to_s
+
+if !user_id.match(/\A\d{1,9}\z/)
+  abort 'Expected User ID'
+end
+
 DB = Sequel.connect('postgres://postgres@postgres:5432/static_website_manager_development')
 
-user = DB[:users].where(id: ARGV).first
+user = DB[:users].where(id: user_id.to_i).first
+command, *args = Shellwords.shellsplit(ENV['SSH_ORIGINAL_COMMAND'].to_s)
 
-if user
-  def exec_git_command(user, git_command, repo_path)
-    exec [git_command, repo_path].join(' ')
-  end
-
-  args = Shellwords.shellsplit(ENV['SSH_ORIGINAL_COMMAND'].to_s)
-
-  if %w[git-upload-pack git-receive-pack git-upload-archive].include?(args[0]) && args.length == 2
-    exec_git_command user, *args
-    exit 0
-  elsif args.empty?
-    $stdout.puts "Hi #{user[:name]}! Static Website Manager does not provide shell access."
-    exit 0
+if user.nil?
+  abort 'No User Found'
+elsif command.nil? || command.empty?
+  puts "Hi #{user[:name]}! You have authenticated successfully, but Static Website Manager does not provide shell access. Goodbye."
+  exit true
+elsif %w[git-upload-pack git-receive-pack git-upload-archive].include?(command)
+  if args.length == 1 && repo_id_match = args[0].match(/\A\/?(\d{1,9})(\.git)?\z/)
+    if website = DB[:websites].where(id: repo_id_match[1].to_i).first
+      if authorization = DB[:authorizations].where(user_id: user[:id], website_id: website[:id]).first
+        exec "#{command} /repos/#{repo_id_match[1]}.git"
+      else
+        abort 'Not Authorized'
+      end
+    else
+      abort 'Not Repository Found'
+    end
   else
-    $stderr.puts 'Not Authorized'
-    exit 1
+    abort "Unsupported Repository Name #{args}"
   end
 else
-  $stderr.puts 'Not Authorized'
-  exit 1
+  abort "Unsupported Command: #{command} #{args}"
 end
