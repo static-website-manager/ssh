66f81a5db710d16d86d1c173a88c898dfcb6e3cc
Theodore Kimble <mail@theodorekimble.com>
2015-10-23 20:56:33 -0700

    Use user_id instead of authentication id

--- authorized_keys.rb
+++ authorized_keys.rb
@@ -6,8 +6,8 @@ DB = Sequel.connect('postgres://postgres@postgres:5432/static_website_manager_de
 
 keys = []
 
-DB[:authentications].to_hash(:id, :public_key).each do |id, public_key|
-  keys << "command=\"/etc/ssh/authorized_actions.rb #{id}\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty #{public_key}"
+DB[:authentications].to_hash(:user_id, :public_key).each do |user_id, public_key|
+  keys << "command=\"/etc/ssh/authorized_actions.rb #{user_id}\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty #{public_key}"
 end
 
 puts keys.join("\n")
