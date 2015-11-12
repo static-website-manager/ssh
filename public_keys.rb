21c07d666fd40543d921c2625ea101fb2af89c1e
Theodore Kimble <mail@theodorekimble.com>
2015-11-12 13:41:50 -0800

    Fix postgres typo

--- public_keys.rb
+++ public_keys.rb
@@ -3,7 +3,7 @@
 require 'sequel'
 
 keys = []
-DB = Sequel.connect('postgres://postgres@postgres:5432/postres')
+DB = Sequel.connect('postgres://postgres@postgres:5432/postgres')
 
 DB[:authentications].to_hash(:user_id, :public_key).each do |user_id, public_key|
   keys << "command=\"/home/git/allowed_actions.rb #{user_id}\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty #{public_key}"
